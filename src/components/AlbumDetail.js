99ea0af0cd8095bedf16931828131027fd348911
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2017-09-10 10:53:10 +0700

    Show image in album list

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
 const AlbumDetail = (props) => {
   const { album } = props
+  const { headerContentStyle, thumbnailStyle } = styles
   return (
     <Card>
       <CardSection>
-        <Text>{album.title}</Text>
+        <View>
+          <Image 
+            style={thumbnailStyle}
+            source={{ uri: album.thumbnail_image }} 
+          />
+        </View>
+        <View style={headerContentStyle} >
+          <Text>{album.title}</Text>
+          <Text>{album.artist}</Text>
+        </View>
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  }
 }
 
 export default AlbumDetail
