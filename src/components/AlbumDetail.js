f7cc11f712f85c4c87d5de3ff978f529817d144f
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2017-09-10 11:09:30 +0700

    Use scrollView for scrolling card album list

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,20 +5,31 @@ import CardSection from './CardSection'
 
 const AlbumDetail = (props) => {
   const { album } = props
-  const { headerContentStyle, thumbnailStyle } = styles
+  const { 
+    headerContentStyle, 
+    thumbnailStyle, 
+    thumbnailContainerStyle, 
+    headerTextStyle,
+    imageStyle
+  } = styles
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image 
             style={thumbnailStyle}
             source={{ uri: album.thumbnail_image }} 
           />
         </View>
         <View style={headerContentStyle} >
-          <Text>{album.title}</Text>
+          <Text style={headerTextStyle}>{album.title}</Text>
           <Text>{album.artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={imageStyle}
+          source={{ uri: album.image }} />
       </CardSection>
     </Card>
   )
@@ -29,9 +40,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18,
+  },
   thumbnailStyle: {
     height: 50,
     width: 50,
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null,
   }
 }
 
